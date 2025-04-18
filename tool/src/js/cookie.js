41ab091ddd3b9c9770b6899c384ce956e2c97823
empiretactics <zalex5052314@gmail.com>
2025-04-19 01:41:34 +0800

    ajax async

--- tool/src/js/cookie.js
+++ tool/src/js/cookie.js
@@ -21,16 +21,3 @@ function getCookie(obj, defaultText = '') {
     // 如果沒有找到，則顯示預設文字
     obj.value = defaultText;
 }
-
-// 時間控制
-function updateDateTime() {
-    const now = new Date();
-    const date = now.toLocaleDateString(); // 當地日期
-    const time = now.toLocaleTimeString(); // 當地時間
-    currentTime = `${date} ${time}`
-    rightBoxTime.innerHTML = currentTime;
-}
-
-// 每秒更新一次
-setInterval(updateDateTime, 1000);
-updateDateTime();
