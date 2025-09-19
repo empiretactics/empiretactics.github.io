ea91888329e07d94db66958be7277e61dc331671
empiretactics <89486423+empiretactics@users.noreply.github.com>
2025-09-20 00:11:18 +0800

    color

--- src/js/color.js
+++ src/js/color.js
@@ -95,7 +95,7 @@ document.head.appendChild(ColorStyle);
 
 const ColorLink = document.createElement("link");
 ColorLink.rel = "stylesheet";
-ColorLink.href = "~/src/css/color.css";
+ColorLink.href = "../src/css/color.css";
 document.head.appendChild(ColorLink);
 
 //main.js
