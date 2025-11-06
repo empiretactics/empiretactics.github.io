e75ecbb1347e428852233301655984a8b3e7eed9
empiretactics <89486423+empiretactics@users.noreply.github.com>
2025-11-06 08:15:39 +0000

    init

--- originP/src/plugin/index.js
+++
@@ -1,8 +0,0 @@
-const Title = document.createElement("title");
-Title.textContent = TitleConfig;
-document.head.appendChild(Title);
-
-const Links = document.getElementById('links');
-Links.innerHTML = LinkConfig.map(link =>
-    `<a href="${link.href}">${link.text}</a><br>`
-).join('');
--- originP/src/plugin/tool.js
+++
@@ -1,9 +0,0 @@
-function filterSpace(arr) {
-    let tmp = [];
-    arr.forEach(v => {
-        if (v.trim() !== '') {
-            tmp.push(v);
-        }
-    });
-    return tmp;
-}
